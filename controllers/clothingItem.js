8bff3c6ca5bf4f34a1b381bb3466b12250ef3266
Ryan Welburn <ryanwelburn64@gmail.com>
2026-05-08 20:40:53 -0600

    Project 15 update 3

--- controllers/clothingItem.js
+++ controllers/clothingItem.js
@@ -1,5 +1,5 @@
 const clothingItem = require("../models/clothingItem");
-const OK, CREATED = require("../utils/errors");
+const { OK, CREATED } = require("../utils/errors");
 const BadRequestError = require("../utils/badrequesterror");
 const NotFoundError = require("../utils/notfounderror");
 const ForbiddenError = require("../utils/forbiddenerror");
